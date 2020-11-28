0a6639a54832ec2a5173a536637c71bc5f114991
FlawlessPT <jotape919@gmail.com>
2020-11-28 01:11:58 +0000

    Finished Footer

--- Footer/footer.php
+++ Footer/footer.php
@@ -1,8 +1,20 @@
+<!-- FIXED FOOTER -->
 <footer class="footer bg-dark text-white py-4 w-100" style="position: fixed; bottom: 0px;">
-    <!-- REMOVE STYLE TAG -->
-    <div class="container">
+    <div class="container text-secondary">
         <div class="row">
-            test-text
+            <div class="col-sm-2">
+                <img class="py-auto float-sm-right" src="../Imagens/Icon/SVG/icon-solid.svg" width=80 alt="logo"><!-- TODO: Change for logo only (remove text) -->
+            </div>
+            <div class="col-xl-5 my-auto text-xl-left text-sm-center">
+                <h4 class="mt-sm-3 ml-xl-n2 font-weight-bold">Park@Home</h4>
+                <h6 class="ml-xl-n2">2020 Park@Home, Todos os direitos reservados.</h6>
+            </div>
+            <div class="col-sm-5 d-flex align-items-xl-center">
+                <button type="button" class="btn flex-fill h-50"><i class="fab fa-linkedin" style="font-size: 25pt; color: white" aria-hidden="true"></i></button>
+                <button type="button" class="btn flex-fill h-50"><i class="fab fa-twitter" style="font-size: 25pt; color: white" aria-hidden="true"></i></button>
+                <button type="button" class="btn flex-fill h-50"><i class="fab fa-facebook" style="font-size: 25pt; color: white" aria-hidden="true"></i></button>
+                <button type="button" class="btn flex-fill h-50"><i class="fab fa-github" style="font-size: 25pt; color: white" aria-hidden="true"></i></button>
+            </div>
         </div>
     </div>
 </footer>
